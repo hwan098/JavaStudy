95448956f59199682300685e6dd98c46318ce7be
LG <LG@DESKTOP-T9VUQBS>
2022-08-25 17:02:42 +0900

    java essence chapter9 exercise9-5

---
+++ JavaStudy2/src/Exercise9_3.java
@@ -0,0 +1,19 @@
+
+public class Exercise9_3 {
+
+	public static void main(String[] args) {
+		String fullPath = "c:\\jdk1.8\\work\\PathSeparateTest.java";
+		String path = "";
+		String fileName = "";
+		
+		path = fullPath.substring(0, 15);	
+		fileName = fullPath.substring(15);
+		
+		System.out.println("fullPath:"+fullPath);
+		System.out.println("path:"+path);
+		System.out.println("fileName:"+fileName);
+
+
+	}
+
+}
