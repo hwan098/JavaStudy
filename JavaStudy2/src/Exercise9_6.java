90987a70c757cd5b8339b11edc012644a6a44694
LG <LG@DESKTOP-T9VUQBS>
2022-08-29 17:17:28 +0900

    exercise9-6

--- JavaStudy2/src/Exercise9_6.java
+++ JavaStudy2/src/Exercise9_6.java
@@ -2,40 +2,30 @@
 public class Exercise9_6 {
 	
 	public static String fillZero(String src, int length) {
-		
-		char[] charArr = new char[length]; //10
-		char[]charArr2 = new char[src.length()];
-		
-		if(src==null || src.length()==length) { //src가 null이거나 length와 길이가 같으면 src반환
+		if(src == null || src.length() == length) {
 			return src;
 		}
 		else if(length <= 0) {
-			return " ";
+			src = "";
+			return src;
 		}
-		
-		else if(length < src.length()) {	//
-			for(int i=0; i<length; i++) {			
-				charArr2[i] =  src.charAt(i);
-			}
-			String changeStr = new String(charArr2);
-			return changeStr;
+		else if(src.length() > length) {
+			src = src.substring(0, length);
+			return src;
 		}
 		else {
-			for(int i=0; i<charArr.length; i++) {			//배열의 각 인덱스에 0삽입
+			char[] charArr = new char[length];
+			char[] charArr2 = new char[length];
+			
+			for(int i=0; i<length; i++) {
 				charArr[i] = '0';
 			}
-			
-			for(int i=0; i<charArr2.length; i++) {
-				charArr2[i] =  src.charAt(i);
-				int insertInNum = length-src.length();
-				charArr[insertInNum + i] = charArr2[i];
-			}
-			
-			String changeStr = new String(charArr);
+			System.arraycopy(charArr, 0, length-src.length(), length, src.length());
+			String changeStr = new String(charArr2);
 			return changeStr;
-		}
-		
+		}	
 	}
+
 
 
 	public static void main(String[] args) {
@@ -58,4 +48,30 @@ for(int j=inNum; j<charArr.length; j++) {
 					charArr[inNum] = src.charAt(k);
 				}	
 			}
+*/
+/*
+ * 
+
+
+
+else if(length < src.length()) {	//
+	for(int i=0; i<length; i++) {			
+		charArr2[i] =  src.charAt(i);
+	}
+	String changeStr = new String(charArr2);
+	return changeStr;
+else {
+	for(int i=0; i<charArr.length; i++) {			//배열의 각 인덱스에 0삽입
+		charArr[i] = '0';
+	}
+	
+	for(int i=0; i<charArr2.length; i++) {
+		charArr2[i] =  src.charAt(i);
+		int insertInNum = length-src.length();
+		charArr[insertInNum + i] = charArr2[i];
+	}
+	
+	String changeStr = new String(charArr);
+	return changeStr;
+}
 */
