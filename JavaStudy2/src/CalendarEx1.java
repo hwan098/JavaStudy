dc969d33a33ebfeea0029595992240f12f9c7a1b
LG <LG@DESKTOP-T9VUQBS>
2022-09-02 16:59:25 +0900

    Chapter 10 ex1

---
+++ JavaStudy2/src/CalendarEx1.java
@@ -0,0 +1,14 @@
+import java.util.Calendar;
+
+public class CalendarEx1 {
+
+	public static void main(String[] args) {
+		Calendar today = Calendar.getInstance();
+		System.out.println(today.get(Calendar.YEAR));
+		System.out.println(today.get(Calendar.MONTH)+1);
+		System.out.println(today.get(Calendar.WEEK_OF_YEAR));;
+		System.out.println(today.get(Calendar.WEEK_OF_MONTH));;
+
+	}
+
+}
---
+++ JavaStudy2/src/Exercise9_14.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.util.regex.*;
+
+public class Exercise9_14 {
+
+	public static void main(String[] args) {
+		String[] phoneNumArr = {
+				"012-3456-7890",
+				"099-2456-7980",
+				"088-2346-9870",
+				"013-3456-7890"
+		};
+		ArrayList list = new ArrayList();
+		Scanner s = new Scanner(System.in);
+		Pattern p = null;
+		
+		
+		while(true) {
+			System.out.print(">>");
+			String input = s.nextLine().trim();
+			
+			if(input.equals("")) {
+				continue;
+			} else if(input.equalsIgnoreCase("Q")) {
+				System.exit(0);
+			  }
+		
+			p = Pattern.compile(".*" + input + ".*");
+			
+			for(int i=0; i < phoneNumArr.length; i++) {
+				Matcher m = p.matcher(phoneNumArr[i]);
+				
+				if(m.matches()) {
+					list.add(phoneNumArr[i]);
+
+				}
+			}
+			
+			if(list.size()>0) {
+				System.out.println(list);
+				list.clear();
+			} else {
+				System.out.println("일치하는 번호가 없습니다."); 
+			  }
+		}
+
+
+	}
+
+}
