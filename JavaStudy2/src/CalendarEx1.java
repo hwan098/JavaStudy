5ba30318fbcb1e52ca9aa1f9d0a6a88715003094
LG <LG@DESKTOP-T9VUQBS>
2022-09-05 16:53:09 +0900

    Java essence chapter 10-15

--- JavaStudy2/src/CalendarEx1.java
+++ JavaStudy2/src/CalendarEx1.java
@@ -6,8 +6,9 @@ public class CalendarEx1 {
 		Calendar today = Calendar.getInstance();
 		System.out.println(today.get(Calendar.YEAR));
 		System.out.println(today.get(Calendar.MONTH)+1);
-		System.out.println(today.get(Calendar.WEEK_OF_YEAR));;
-		System.out.println(today.get(Calendar.WEEK_OF_MONTH));;
+		System.out.println(today.get(Calendar.DAY_OF_MONTH));
+		System.out.println(today.get(Calendar.WEEK_OF_YEAR));
+		System.out.println(today.get(Calendar.WEEK_OF_MONTH));
 
 	}
 
